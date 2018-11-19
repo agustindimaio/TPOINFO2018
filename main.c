6c3841f322b5b77d098f70925248c6a7ca47a4ee
agustindimaio <42102096+agustindimaio@users.noreply.github.com>
2018-11-19 13:52:43 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,103 @@
+/*
+===============================================================================
+ Name        : TPO_QUIZA_FINAL.c
+ Author      : $(author)
+ Version     :
+ Copyright   : $(copyright)
+ Description : main definition
+===============================================================================
+*/
+
+#ifdef __USE_CMSIS
+#include "LPC17xx.h"
+#endif
+
+#include "Aplicacion.h"
+#include "main.h"
+
+char PuntosALCD[10];
+char PuntosBLCD[10];
+char TiempoPartidoLCD[10];
+volatile uint8_t PuntosA;
+volatile uint8_t PuntosB;
+
+//volatile uint8_t BufferEntradas;		//Buffer de Entradas Digitales
+
+int A=000;
+
+int main(void)
+{
+	uint8_t i;
+	uint8_t r[8];
+
+	Inicializar();
+
+	SetPINSEL(PDIN,0x00);
+	SetPINSEL(PCLK,0x00);
+	SetPINSEL(PCS,0x00);
+
+	SetDIR(PDIN,SALIDA);
+	SetDIR(PCLK,SALIDA);
+	SetDIR(PCS,SALIDA);
+
+	// Inicializo
+	sendCmd(SHDOWN,NOSHUT);
+	sendCmd(SLIMIT,MAXSCAN);
+	sendCmd(INTEN,MINTEN);
+	sendCmd(DEMODE,NODECO);
+	sendCmd(DSPTEST,NOOP);
+
+	stringConverter('r', r);
+
+	for(i=0;i<8;i++)
+	{
+		sendCmd( (DIGIT+i) , r[i] );
+	}
+
+    while(1)
+    {
+    	//Aplicacion();
+    	//TimerEvent();
+    }
+    return 0 ;
+}
+
+void Delay(uint8_t time)
+{
+	for(uint8_t j = 0; j < time; j++);
+}
+
+void stringConverter(uint8_t l,uint8_t msg[])
+{
+	uint8_t i;
+	int aux=l;
+	aux=(aux-32)+(7*(aux-32));
+	for(i=0;i<8;i++)
+	{
+		msg[i]=ucFont[aux+i];
+	}
+}
+
+void sendCmd(uint8_t address, uint8_t data)
+{
+	uint8_t i, j;
+	uint16_t frame= ( (address<<(0x08)) | (data) );
+
+	SetPIN(PCLK,AP);
+	SetPIN(PDIN,AP);
+	SetPIN(PCS,AP);
+
+	for(j=0;j<4;j++)
+	{
+		for(i=0;i<16;i++)
+		{
+			SetPIN( PDIN, !((frame>>(15-i)) & (0x01)) );
+			Delay(10);
+			SetPIN(PCLK,EN);
+			Delay(10);
+			SetPIN(PCLK,AP);
+			Delay(10);
+		}
+	}
+	SetPIN(PCS,EN);
+}
