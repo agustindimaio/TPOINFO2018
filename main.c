209a17a446e7cca8f73e10e4c0c78ea7c1628205
agustindimaio <42102096+agustindimaio@users.noreply.github.com>
2018-11-25 09:16:46 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -27,10 +27,38 @@ int A=000;
 
 int main(void)
 {
-	uint8_t i;
-	uint8_t r[8];
+	uint8_t i, j;
+	uint8_t n[4][8];
+	uint8_t str[5]="hola";
 
 	Inicializar();
+
+	InitMatrix();
+
+	for(i=0;i<8;i++)
+	{
+		for(j=0;j<4;j++)
+		{
+			stringConverter(j, str[j], n);
+			sendCmd( (DIGIT+i) , n[j][i] );
+		}
+	}
+
+    while(1)
+    {
+    	//Aplicacion();
+    	//TimerEvent();
+    }
+}
+
+void Delay(uint8_t time)
+{
+	for(uint8_t j = 0; j < time; j++);
+}
+
+void InitMatrix(void)
+{
+	uint32_t j;
 
 	SetPINSEL(PDIN,0x00);
 	SetPINSEL(PCLK,0x00);
@@ -40,64 +68,42 @@ int main(void)
 	SetDIR(PCLK,SALIDA);
 	SetDIR(PCS,SALIDA);
 
-	// Inicializo
-	sendCmd(SHDOWN,NOSHUT);
-	sendCmd(SLIMIT,MAXSCAN);
-	sendCmd(INTEN,MINTEN);
-	sendCmd(DEMODE,NODECO);
-	sendCmd(DSPTEST,NOOP);
-
-	stringConverter('r', r);
-
-	for(i=0;i<8;i++)
-	{
-		sendCmd( (DIGIT+i) , r[i] );
-	}
-
-    while(1)
-    {
-    	//Aplicacion();
-    	//TimerEvent();
-    }
-    return 0 ;
+	for(j=0;j<4;j++) { sendCmd(SHDOWN,SHUTDO); }
+	for(j=0;j<4;j++) { sendCmd(SHDOWN,NOSHUT); }
+	for(j=0;j<4;j++) { sendCmd(SLIMIT,MAXSCAN); }
+	for(j=0;j<4;j++) { sendCmd(INTEN,MINTEN); }
+	for(j=0;j<4;j++) { sendCmd(DEMODE,NODECO); }
+	for(j=0;j<4;j++) { sendCmd(DSPTEST,NOROP); }
 }
 
-void Delay(uint8_t time)
-{
-	for(uint8_t j = 0; j < time; j++);
-}
-
-void stringConverter(uint8_t l,uint8_t msg[])
+void stringConverter(uint8_t j, uint8_t l, uint8_t msg[][8])
 {
 	uint8_t i;
 	int aux=l;
 	aux=(aux-32)+(7*(aux-32));
 	for(i=0;i<8;i++)
 	{
-		msg[i]=ucFont[aux+i];
+		msg[j][i]=ucFont[aux+i];
 	}
 }
 
-void sendCmd(uint8_t address, uint8_t data)
+void sendCmd(uint8_t address, uint8_t data)	// Envía una trama
 {
-	uint8_t i, j;
+	uint8_t i;
 	uint16_t frame= ( (address<<(0x08)) | (data) );
 
 	SetPIN(PCLK,AP);
 	SetPIN(PDIN,AP);
 	SetPIN(PCS,AP);
 
-	for(j=0;j<4;j++)
+	for(i=0;i<16;i++)
 	{
-		for(i=0;i<16;i++)
-		{
-			SetPIN( PDIN, !((frame>>(15-i)) & (0x01)) );
-			Delay(10);
-			SetPIN(PCLK,EN);
-			Delay(10);
-			SetPIN(PCLK,AP);
-			Delay(10);
-		}
+		SetPIN( PDIN, ( ~(frame>>(15-i)) & (0x01)) );
+		Delay(10);
+		SetPIN(PCLK,EN);
+		Delay(10);
+		SetPIN(PCLK,AP);
+		Delay(10);
 	}
 	SetPIN(PCS,EN);
 }
